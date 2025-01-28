a0e9339edb7521b9454dd987b3bdedcdafdc580f
JJC <josh@jjcasswell.com>
2025-01-28 01:18:28 +0000

    tweaks for performance and display

--- cardputer/SkiTracker.py
+++ cardputer/SkiTracker.py
@@ -13,15 +13,18 @@ from font import vga2_16x32 as font
 
 
 try:
-    from SkiTracker_utils import make_filename, gps_string, csv_string, utc_string, float_string, display_values
+    from SkiTracker_utils import make_filename, gps_string, csv_string, utc_string, float_string
 except ImportError:
-    from apps.Skitracker.SkiTracker_utils import make_filename, gps_string, csv_string, utc_string, float_string, display_values
+    from apps.Skitracker.SkiTracker_utils import make_filename, gps_string, csv_string, utc_string, float_string
 
-
+config = Config()
+BG_COLOUR = config.palette[2]
+FG_COLOUR = config.palette[10]
 
 class SkiData():
 	
 	numRecords=10
+	totalRecords=0
 	records=[]
 	path = '/sd/ski_data/'
 	
@@ -29,15 +32,31 @@ class SkiData():
 		self.filename=self.path + filename
 		print(self.filename)
 		
-	def add_data(self,data):
-		self.records.append(data)
+	def add_data(self,gps):
+		lat_string = gps_string(gps.latitude(coord_format=as_GPS.DD))
+		lon_string = gps_string(gps.longitude(coord_format=as_GPS.DD))
+		items=[lat_string,
+               lon_string,
+               float_string(gps.altitude),utc_string(gps.utc)]
+        
+		self.records.append(csv_string(items))
 		
 		if len(self.records) > self.numRecords:
 			self.save_data()
+			
+			self.totalRecords+=self.numRecords
+			
+			stats=[float_string(gps.pdop),
+                   float_string(gps.hdop),
+                   float_string(gps.vdop),
+                   float_string(gps.satellites_in_use)]
+			stats_string = csv_string(stats)
+			#print(csv_string(items))
+			display_values(lat_string,lon_string,stats_string,str(self.totalRecords))
 		
 	def save_data(self):
 		
-		print('saving data')
+		#print('saving data')
 		output = '\n'.join(self.records) + '\n'
 		self.records=[]
 		
@@ -46,34 +65,46 @@ class SkiData():
 			#fp.flush()
 			
 def callback(gps, *_):  # Runs for each valid fix
-	lat_string = gps_string(gps.latitude(coord_format=as_GPS.DD))
-	lon_string = gps_string(gps.longitude(coord_format=as_GPS.DD))
-	
-	items=[lat_string,
-		   lon_string,
-		   float_string(gps.altitude),utc_string(gps.utc)]
-	
-	stats=[float_string(gps.pdop),
-		   float_string(gps.hdop),
-		   float_string(gps.vdop),
-		   float_string(gps.satellites_in_use)]
-	
-	stats_string = csv_string(stats)
-	
-	print(csv_string(items))
-	data.add_data(csv_string(items))
-	
-	display_values(display, bg_colour,fg_colour,font, lat_string,lon_string,stats_string)
+    data.add_data(gps)
+
+def display_values(lat_string,lon_string,stats_string,records):
+    
+    display.fill(BG_COLOUR)
+    display.text(
+            lat_string,
+            0, #X
+            0, # Y
+            FG_COLOUR,
+            font=font
+            )
+    display.text(
+            lon_string,
+            0, #X
+            font.HEIGHT * 1, # Y
+            FG_COLOUR,
+            font=font
+            )
+    display.text(
+            stats_string,
+            0, #X
+            font.HEIGHT * 2, # Y
+            FG_COLOUR,
+            font=font
+            )
+    display.text(
+            records,
+            0, #X
+            font.HEIGHT * 3, # Y
+            FG_COLOUR,
+            font=font
+            )
+    display.show()
 
 sd = SDCard(slot=2, sck=Pin(40), miso=Pin(39), mosi=Pin(14), cs=Pin(12))
 uart=UART(1,baudrate=115200,bits=8,parity=None,stop=1,tx=Pin(2),rx=Pin(1))
 
 display=Display(use_tiny_buf=True)
-config = Config()
-bg_colour = config.palette[2]
-fg_colour = config.palette[10]
-
-display.fill(bg_colour)
+display.fill(BG_COLOUR)
 
 sreader = asyncio.StreamReader(uart)  # Create a StreamReader
 gps = as_GPS.AS_GPS(sreader, fix_cb=callback)  # Instantiate GPS
@@ -85,10 +116,25 @@ data = SkiData(filename=fname)
 os.mount(sd, '/sd')
 
 async def run_loop():
-	
-	print('waiting for GPS data')
-	while True:
-		await gps.data_received(position=True, altitude=True)
+    
+    display.text(
+        'SkiTracker v3',
+        0, #X
+        0, # Y
+        FG_COLOUR,
+        font=font
+        )
+    display.text(
+        'Awaiting GPS...',
+        0, #X
+        font.HEIGHT * 1, # Y
+        FG_COLOUR,
+        font=font
+        )
+    display.show()
+    #print('waiting for GPS data')
+    while True:
+        await gps.data_received(position=True, altitude=True)
 
 try:
 	asyncio.run(run_loop())
@@ -97,7 +143,7 @@ except KeyboardInterrupt:
 finally:
 	os.umount('/sd')
 
-#from machine import SDCard, UART, Pin,
+#from machine import SDCard, UART, Pin
 #sd = SDCard(slot=2, sck=Pin(40), miso=Pin(39), mosi=Pin(14), cs=Pin(12))
 #import os; os.mount(sd, '/sd')
 #os.umount('/sd')
